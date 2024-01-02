5dc6ace52ce0b89126d8106b18337b1a617b88ab
amit-ai <dubeyamit219@gmail.com>
2024-01-02 13:14:02 +0530

    refactor class to className

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,24 +22,24 @@ export default function Login() {
 
     return (
         <div className="mt-20">
-            <section class="bg-gray-5" onSubmit={handleSignin}>
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto mt-5">
+            <section className="bg-gray-5" onSubmit={handleSignin}>
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto mt-5">
                     <a
                         href="#"
-                        class="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+                        className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
                     >
                         JobListing
                     </a>
-                    <div class="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0 ">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                    <div className="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0 ">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                                 Sign in to your account
                             </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label
                                         for="email"
-                                        class="block mb-2 text-sm font-medium text-gray-900 "
+                                        className="block mb-2 text-sm font-medium text-gray-900 "
                                     >
                                         Your email
                                     </label>
@@ -47,7 +47,7 @@ export default function Login() {
                                         type="email"
                                         name="email"
                                         id="email"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="name@company.com"
                                         required=""
                                         onChange={(e) =>
@@ -58,7 +58,7 @@ export default function Login() {
                                 <div>
                                     <label
                                         for="password"
-                                        class="block mb-2 text-sm font-medium text-gray-900"
+                                        className="block mb-2 text-sm font-medium text-gray-900"
                                     >
                                         Password
                                     </label>
@@ -67,28 +67,28 @@ export default function Login() {
                                         name="password"
                                         id="password"
                                         placeholder="••••••••"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                         required=""
                                         onChange={(e) =>
                                             setPassword(e.target.value)
                                         }
                                     />
                                 </div>
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-start">
-                                        <div class="flex items-center h-5">
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
                                             <input
                                                 id="remember"
                                                 aria-describedby="remember"
                                                 type="checkbox"
-                                                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
                                                 required=""
                                             />
                                         </div>
-                                        <div class="ml-3 text-sm">
+                                        <div className="ml-3 text-sm">
                                             <label
                                                 for="remember"
-                                                class="text-gray-500 "
+                                                className="text-gray-500 "
                                             >
                                                 Remember me
                                             </label>
@@ -96,22 +96,22 @@ export default function Login() {
                                     </div>
                                     <a
                                         href="#"
-                                        class="text-sm font-medium text-blue-600 hover:underline "
+                                        className="text-sm font-medium text-blue-600 hover:underline "
                                     >
                                         Forgot password?
                                     </a>
                                 </div>
                                 <button
                                     type="submit"
-                                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                                    className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                                 >
                                     Sign in
                                 </button>
-                                <p class="text-sm font-light text-gray-500 ">
+                                <p className="text-sm font-light text-gray-500 ">
                                     Don't have an account yet?{" "}
                                     <Link
                                         to={"/signup"}
-                                        class="font-medium text-blue-600 hover:underline "
+                                        className="font-medium text-blue-600 hover:underline "
                                     >
                                         Sign up
                                     </Link>
